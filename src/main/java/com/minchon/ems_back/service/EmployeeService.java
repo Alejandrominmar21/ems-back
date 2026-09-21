d5a38394b5f8c5ce22ce5a4be3108272be6d8ebf
Alejandro <alejandrominmar@gmail.com>
2026-09-21 09:36:10 +0200

    Feat: EmployeeService

---
+++ src/main/java/com/minchon/ems_back/service/EmployeeService.java
@@ -0,0 +1,8 @@
+package com.minchon.ems_back.service;
+
+import com.minchon.ems_back.dto.EmployeeDto;
+
+public interface EmployeeService {
+    EmployeeDto createEmplyee(EmployeeDto employeeDto);
+
+}
---
+++ src/main/java/com/minchon/ems_back/service/impl/EmployeeServiceImp.java
@@ -0,0 +1,23 @@
+package com.minchon.ems_back.service.impl;
+
+import com.minchon.ems_back.dto.EmployeeDto;
+import com.minchon.ems_back.entity.Employee;
+import com.minchon.ems_back.mapper.EmployeeMapper;
+import com.minchon.ems_back.repository.EmployeeRepository;
+import com.minchon.ems_back.service.EmployeeService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class EmployeeServiceImp implements EmployeeService {
+
+    private EmployeeRepository employeeRepository;
+    @Override
+    public EmployeeDto createEmplyee(EmployeeDto employeeDto) {
+        Employee employee = EmployeeMapper.mapToEmployee(employeeDto);
+        Employee savedEmployee =  employeeRepository.save(employee);
+
+        return EmployeeMapper.mapToEmployeeDto(savedEmployee);
+    }
+}
